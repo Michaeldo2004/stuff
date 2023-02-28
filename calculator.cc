e5f48940b424e507fb4615e670e4d25366e9227b
Michaeldo2004 <123520368+Michaeldo2004@users.noreply.github.com>
2023-02-27 22:06:57 -0800

    Update calculator.cc

--- calculator.cc
+++ calculator.cc
@@ -7,63 +7,51 @@ double divide(int &x, int &y){
 }
 
 void calculator(int& x, int& y, std::string& action){
-  bool rightInput = false;
-  while(!rightInput){
+  while(true){
     int input = 0;
     if(action == "add"){             //Addition
       std::cout << "\nIf you add these numbers, you will get " 
                 << x+y << ".\n";
-      rightInput=true;
       break;
     }else if(action == "subtract"){  //Subtraction
-      bool subTrue = false;
-      while(!subTrue){
+      while(true){
         std::cout << "\nWhich number will be the minuend? ";
         std::cin >> input;
         if(input == x){
           std::cout << "\nIf you subtract these numbers, you will get "
                     << x-y << ".\n";
-          subTrue=true;
-          rightInput=true;
           break;
         }else if(input == y){
           std::cout << "\nIf you subtract these numbers, you will get "
-                    << y-x << ".\n";
-          subTrue=true;
-          rightInput=true;
+                    << y-x << ".\n";;
           break;
         } else{
         std::cout<< "\nInvalid input.\n";
           }
+        break;
       }
     }else if(action == "multiply"){  //Multiplication
       std::cout << "\nIf you multiply these numbers, you will get " << x*y << ".\n";
-      rightInput=true;
       break;
     }else if(action == "divide"){    //Division
-      bool divTrue = false;
-      while(!divTrue){
+      while(true){
         std::cout << "\nWhich number will be the dividend? ";
         std::cin >> input;
         if(input == x){
           std::cout<< "\nIf you divide these numbers, you will get "<<divide(x,y) << ".\n";
-          divTrue=true;
-          rightInput=true;
           break;
         }else if(input == y){
           std::cout<< "\nIf you subtract these numbers, you will get "<< divide(y,x) << ".\n";
-          divTrue=true;
-          rightInput=true;
           break;
         }else{
         std::cout<< "\nInvalid input.\n";
           }
+        break;
       }
     }
     std::cout<< "Your input is invalid, try again.\nWould you like "
              << "these numbers to 'add', 'subtract', 'multiply', or 'divide'? ";
     std::cin >> action;
     calculator(x, y , action);
-    
     }
     }
