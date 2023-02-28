82a747d4c241c4160e3d6c2ae1a53e163081e0bf
Michaeldo2004 <123520368+Michaeldo2004@users.noreply.github.com>
2023-02-27 22:17:35 -0800

    Update calculator.cc

--- calculator.cc
+++ calculator.cc
@@ -1,18 +1,12 @@
 #include <iostream>
 #include <iomanip>
 
-//functions for division
-double divide(int &x, int &y){
-  return (double) (x/y);
-}
 
 void calculator(int& x, int& y, std::string& action){
-  while(true){
     int input = 0;
     if(action == "add"){             //Addition
       std::cout << "\nIf you add these numbers, you will get " 
                 << x+y << ".\n";
-      break;
     }else if(action == "subtract"){  //Subtraction
       while(true){
         std::cout << "\nWhich number will be the minuend? ";
@@ -28,30 +22,23 @@ void calculator(int& x, int& y, std::string& action){
         } else{
         std::cout<< "\nInvalid input.\n";
           }
-        break;
       }
     }else if(action == "multiply"){  //Multiplication
       std::cout << "\nIf you multiply these numbers, you will get " << x*y << ".\n";
-      break;
     }else if(action == "divide"){    //Division
       while(true){
         std::cout << "\nWhich number will be the dividend? ";
         std::cin >> input;
         if(input == x){
-          std::cout<< "\nIf you divide these numbers, you will get "<<divide(x,y) << ".\n";
+          std::cout<< "\nIf you divide these numbers, you will get "<< (double) (x/y) << ".\n";
           break;
         }else if(input == y){
-          std::cout<< "\nIf you subtract these numbers, you will get "<< divide(y,x) << ".\n";
+          std::cout<< "\nIf you subtract these numbers, you will get "<< (double) (y/x) << ".\n";
           break;
         }else{
         std::cout<< "\nInvalid input.\n";
           }
-        break;
       }
     }
-    std::cout<< "Your input is invalid, try again.\nWould you like "
-             << "these numbers to 'add', 'subtract', 'multiply', or 'divide'? ";
-    std::cin >> action;
-    calculator(x, y , action);
     }
-    }
+    
